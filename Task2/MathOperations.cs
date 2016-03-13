6d6aca1cfd9a1d52d3cc1743ac419768555e3d45
StepanLyalkovski <stepanlyalkovski@gmail.com>
2016-03-14 02:08:22 +0300

    add Gcd arguments check

--- Task2/MathOperations.cs
+++ Task2/MathOperations.cs
@@ -11,10 +11,18 @@ namespace Task2
         /// <summary>
         /// Return the greatest common divisor (GCD) of two numbers
         /// </summary>
-        static int Gcd(int number1, int number2)
+        private static int Gcd(int number1, int number2)
         {
-            //Debug.WriteLine($"{number1} {number2}");
-            return number2 == 0 ? number1 : Gcd(number2, number1 % number2);
+            if (number1 == 0 || number2 == 0)
+                throw new ArgumentException("One of the arguments is 0");
+            while (true)
+            {                
+                Debug.WriteLine($"{number1} {number2}");
+                if (number2 == 0) return number1;
+                int temp = number1;
+                number1 = number2;
+                number2 = temp % number2;
+            }
         }
 
         /// <summary>
@@ -33,7 +41,13 @@ namespace Task2
             return result;
         }
 
-        public static int Gcd(params int[] numbers) => numbers.Aggregate(Gcd);
+        public static int Gcd(params int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+                throw new ArgumentException("Number of arguments is empty or equals null");
+
+            return numbers.Aggregate(Gcd);
+        } 
 
         public static int Gcd(out Stopwatch time, params int[] numbers)
         {
