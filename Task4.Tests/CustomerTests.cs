7d9996eb40c9d5440f7f32168a69b23e44db3f79
StepanLyalkovski <stepanlyalkovski@gmail.com>
2016-03-14 15:29:55 +0300

    add Customer tests

---
+++ Task4.Tests/CustomerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace Task4.Tests
+{
+    [TestFixture]
+    public class CustomerTests
+    {
+        private Customer cust = new Customer("Jeffrey Richter", "+1 (425) 555-0100", 1000000);
+
+        [Test]
+        //[TestCase(null, Result = "Jeffrey Richter,  1,000,000.00, +1 (425) 555-0100")]
+        [TestCase("T", Result = "+1 (425) 555-0100")]
+        [TestCase("NR", Result = "Jeffrey Richter, 1,000,000.00")]
+        [TestCase("R", Result = "1000000")]
+        public string GetCustomer_Format(string format)
+        {
+            return cust.ToString(format);
+
+        }
+    }
+}
