717dde85168a76fac8b1db903db7877cd72a508b
StepanLyalkovski <stepanlyalkovski@gmail.com>
2016-03-14 10:42:05 +0300

    add BinaryGcd , refactor Euclidean Gcd

--- Task2/MathOperations.cs
+++ Task2/MathOperations.cs
@@ -13,16 +13,8 @@ namespace Task2
         /// </summary>
         private static int Gcd(int number1, int number2)
         {
-            if (number1 == 0 || number2 == 0)
-                throw new ArgumentException("One of the arguments is 0");
-            while (true)
-            {                
-                Debug.WriteLine($"{number1} {number2}");
-                if (number2 == 0) return number1;
-                int temp = number1;
-                number1 = number2;
-                number2 = temp % number2;
-            }
+            //Debug.WriteLine($"{number1} {number2}");
+            return number2 == 0 ? number1 : Gcd(number2, number1 % number2);
         }
 
         /// <summary>
@@ -56,5 +48,46 @@ namespace Task2
             time.Stop();
             return result;
         }
+
+        public static int GcdBinary(int number1, int number2)
+        {
+            if (number1 < 0) number1 *= -1;
+            if (number2 < 0) number2 *= -1;
+                
+            if (number1 == 0) return number2;
+            if (number2 == 0) return number1;
+            if (number1 == number2) return number1;
+
+            bool value1IsEven = (number1 & 1) == 0;
+            bool value2IsEven = (number2 & 1) == 0;
+
+            if (value1IsEven && value2IsEven)
+                return GcdBinary(number1 >> 1, number2 >> 1) << 1;
+            else if (value1IsEven && !value2IsEven)
+                return GcdBinary(number1 >> 1, number2);
+            else if (value2IsEven)
+                return GcdBinary(number1, number2 >> 1);
+            else if (number1 > number2)
+                return GcdBinary((number1 - number2) >> 1, number2);
+            else
+                return GcdBinary(number1, (number2 - number1) >> 1);
+        }
+
+        public static int GcdBinary(params int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+                throw new ArgumentException("Number of arguments is empty or equals null");
+
+            return numbers.Aggregate(GcdBinary);
+        }
+
+        public static int GcdBinary(out Stopwatch time, params int[] numbers)
+        {
+            time = Stopwatch.StartNew();
+            int result = GcdBinary(numbers);
+            time.Stop();
+            return result;
+        }
+        
     }
 }
