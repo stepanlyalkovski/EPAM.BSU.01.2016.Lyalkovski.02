11eaa6b8f6bb846aaf1f049020f43cc9d3393076
StepanLyalkovski <stepanlyalkovski@gmail.com>
2016-03-14 15:36:21 +0300

    add more tests to Customer

--- Task4.Tests/CustomerTests.cs
+++ Task4.Tests/CustomerTests.cs
@@ -13,10 +13,11 @@ namespace Task4.Tests
         private Customer cust = new Customer("Jeffrey Richter", "+1 (425) 555-0100", 1000000);
 
         [Test]
-        //[TestCase(null, Result = "Jeffrey Richter,  1,000,000.00, +1 (425) 555-0100")]
+        [TestCase(null, Result = "Jeffrey Richter, 1,000,000.00, +1 (425) 555-0100")]   
         [TestCase("T", Result = "+1 (425) 555-0100")]
         [TestCase("NR", Result = "Jeffrey Richter, 1,000,000.00")]
         [TestCase("R", Result = "1000000")]
+        [TestCase("F", Result = "Jeffrey Richter, 1,000,000.00, +1 (425) 555-0100")]
         public string GetCustomer_Format(string format)
         {
             return cust.ToString(format);
