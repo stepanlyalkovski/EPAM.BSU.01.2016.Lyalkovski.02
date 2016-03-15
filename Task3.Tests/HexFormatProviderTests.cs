fbc8aa833bb74420ded495143900110565daf41b
StepanLyalkovski <stepanlyalkovski@gmail.com>
2016-03-16 00:10:10 +0300

    add required HexFormatProvider tests

--- Task3.Tests/HexFormatProviderTests.cs
+++ Task3.Tests/HexFormatProviderTests.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using  Task3;
@@ -31,6 +33,32 @@ namespace Task3.Tests
         {
             IFormatProvider hProvider = new HexFormatProvider();
             return string.Format(hProvider, format, number);
+        }
+
+        [TestCase(145, Result = "0x91")]
+        [TestCase(-145, Result = "-0x91")]
+        [TestCase(0, Result = "0x0")]
+        [TestCase(41837, Result = "0xA36D")]
+        [TestCase(47, Result = "0x2F")]
+        [TestCase(47.2, ExpectedException = typeof(ArgumentException))]
+        public string Format_Test(object number)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            IFormatProvider fp = new HexFormatProvider();
+            return string.Format(fp, "{0:H}", number);
+        }
+
+        [TestCase(47, "{0:X}", Result = "2F")]
+        [TestCase(.473, "{0:P}", Result = "47.30 %")]
+        [TestCase(.473, "{0:P0}", Result = "47 %")]
+        [TestCase(4.73, "{0:C}", Result = "¤4.73")]
+        [TestCase(4.73, "{0:C}", Result = "¤4.73")]
+        [TestCase(4.7321, "{0:F2}", Result = "4.73")]
+        public string ParentFormat_Test(object number, string format)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            IFormatProvider fp = new HexFormatProvider();
+            return string.Format(fp, format, number);
         }
 
 
